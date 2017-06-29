a947dac8b7b7453898edfee7c27a0989baefac5c
Keith Wygant <kawygant@ncsu.edu>
2017-06-29 16:54:56 -0400

    made private functions local.

--- fac.el
+++ fac.el
@@ -4,70 +4,75 @@
 (mapc #'require
       [hook-up primary-pane miscellaneous])
 
+(cl-labels
+;;; Private Functions:
+
 ;;; Colors (should be somewhere else, like `colors.el'...)
-
-(defun fac--blend-colors (C1 C2)
-  "(R G B) -> (R G B) -> (R G B)
+    ((blend-colors
+      (C1 C2)
+      "(R G B) -> (R G B) -> (R G B)
     Evenly blend C1 and C2, two emacs RGB triplets."
-  (declare (pure t) (side-effect-free t))
-  (seq-mapn (lambda (X Y) (* 0.5 (+ X Y)))
-            C1 C2))
+      (declare (pure t) (side-effect-free t))
+      (seq-mapn (lambda (X Y) (* 0.5 (+ X Y)))
+                C1 C2))
 
-(defun fac--intensify-color (COLOR REFERENCE)
-  "(R G B) -> (R G B) -> (R G B)
+     (intensify-color
+      (COLOR REFERENCE)
+      "(R G B) -> (R G B) -> (R G B)
     Shift COLOR away from REFERENCE."
-  (declare (pure t) (side-effect-free t))
-  (seq-mapn (lambda (C R)
-              (* 0.5 (+ C (if (> C R) 1 0))))
-            COLOR REFERENCE))
+      (declare (pure t) (side-effect-free t))
+      (seq-mapn (lambda (C R)
+                  (* 0.5 (+ C (if (> C R) 1 0))))
+                COLOR REFERENCE)))
+;;; Public Functions:
 
 ;;; Basic face stuff
 
-(defun fac-select (&rest FONTS)
-  "Return the first available font in FONTS, or the default font if none are available."
-  (umr-if (null FONTS) (face-attribute 'default :family)
-          (member (car FONTS) (fac-family-list)) (car FONTS)
-          (apply #'fac-select (cdr FONTS))))
+  (defun fac-select (&rest FONTS)
+    "Return the first available font in FONTS, or the default font if none are available."
+    (umr-if (null FONTS) (face-attribute 'default :family)
+            (member (car FONTS) (fac-family-list)) (car FONTS)
+            (apply #'fac-select (cdr FONTS))))
 
-(defun fac-def-faces (GROUP &rest FACES)
-  "Create FACES (name docstring properties) in GROUP. No fancy business here; the display is always t."
-  (declare (indent 1))
-  (cl-loop
-   for (name docstring . properties) in FACES
-   do (custom-declare-face name `((t . ,properties)) docstring :group GROUP)))
+  (defun fac-def-faces (GROUP &rest FACES)
+    "Create FACES (name docstring properties) in GROUP. No fancy business here; the display is always t."
+    (declare (indent 1))
+    (cl-loop
+     for (name docstring . properties) in FACES
+     do (custom-declare-face name `((t . ,properties)) docstring :group GROUP)))
 
-(defun fac-set-attributes (&rest FACES)
-  "From FACES of (face :attr-1 a1 :attr-2 a2 ...) lists, give each face its attributes. Create undefined faces."
-  (cl-loop
-   for (face . attributes) in FACES
-   do
-   ;; (unless (facep face) (make-face face))
-   (apply #'set-face-attribute face nil attributes)))
+  (defun fac-set-attributes (&rest FACES)
+    "From FACES of (face :attr-1 a1 :attr-2 a2 ...) lists, give each face its attributes. Create undefined faces."
+    (cl-loop
+     for (face . attributes) in FACES
+     do
+     ;; (unless (facep face) (make-face face))
+     (apply #'set-face-attribute face nil attributes)))
 
-(defun fac-shift-foreground (FUNCTION FACE REFERENCE)
-  "Set FACE's foreground to the result of applying FUNCTION to REFERENCE's foreground and background."
-  (umr-let
-   color-of ((KEY)
-             (color-name-to-rgb (face-attribute REFERENCE KEY nil 'default)))
-   (set-face-attribute
-    FACE
-    nil
-    :foreground (apply #'color-rgb-to-hex
-                       (funcall FUNCTION
-                                (color-of :foreground)
-                                (color-of :background))))))
+  (defun fac-shift-foreground (FUNCTION FACE REFERENCE)
+    "Set FACE's foreground to the result of applying FUNCTION to REFERENCE's foreground and background."
+    (umr-let
+     color-of ((KEY)
+               (color-name-to-rgb (face-attribute REFERENCE KEY nil 'default)))
+     (set-face-attribute
+      FACE
+      nil
+      :foreground (apply #'color-rgb-to-hex
+                         (funcall FUNCTION
+                                  (color-of :foreground)
+                                  (color-of :background))))))
 
-(defun fac-fade-foreground (FACE REFERENCE)
-  "Make FACE's foreground a less intense version of REFERENCE's.
+  (defun fac-fade-foreground (FACE REFERENCE)
+    "Make FACE's foreground a less intense version of REFERENCE's.
 REFERENCE is used to avoid fading FACE into oblivion with repreated applications."
-  (fac-shift-foreground #'fac--blend-colors FACE REFERENCE))
+    (fac-shift-foreground #'blend-colors FACE REFERENCE))
 
-(defun fac-intensify-foreground (FACE REFERENCE)
-  (fac-shift-foreground #'fac--intensify-color FACE REFERENCE))
+  (defun fac-intensify-foreground (FACE REFERENCE)
+    (fac-shift-foreground #'intensify-color FACE REFERENCE))
 
 ;;; Adaptive faces
 
-(defvar fac-adaptive-faces-setup ()
+  (defvar fac-adaptive-faces-setup ()
     "a list of adaptive face setup functions")
 
   (defun fac-def-adaptive-faces (GROUP &rest ADAPTIVE-FACES)
@@ -107,25 +112,25 @@ FACE-SETUP should a procedure of 2 arguments (faces) that sets attributes of the
      (seq-doseq (f ADAPTIVE-FACES)
        (apply #'def-adaptive-face f))))
 
-(defun fac-reset-adaptive-faces ()
-  (run-hooks 'fac-adaptive-faces-setup))
+  (defun fac-reset-adaptive-faces ()
+    (run-hooks 'fac-adaptive-faces-setup))
 
-(unless (boundp 'after-load-theme-hook)
-  (hook-up-make-hook :after load-theme))
+  (unless (boundp 'after-load-theme-hook)
+    (hook-up-make-hook :after load-theme))
 
-(hook-up [after-load-theme-hook]
-         [fac-reset-adaptive-faces])
+  (hook-up [after-load-theme-hook]
+           [fac-reset-adaptive-faces])
 
 
-(defun fac-box->lines (FACE)
-  "Turn a box into under- and over-lines."
-  (umr-let
-   color (pcase (face-attribute FACE :box)
-           (`nil nil)
-           (`t (face-attribute 'default :color))
-           ((and (pred stringp) c) c)
-           (plist (plist-get plist :color)))
-   (when color (set-face-attribute
-                FACE nil :box nil :underline color :overline color))))
+  (defun fac-box->lines (FACE)
+    "Turn a box into under- and over-lines."
+    (umr-let
+     color (pcase (face-attribute FACE :box)
+             (`nil nil)
+             (`t (face-attribute 'default :color))
+             ((and (pred stringp) c) c)
+             (plist (plist-get plist :color)))
+     (when color (set-face-attribute
+                  FACE nil :box nil :underline color :overline color)))))
 
 (provide 'fac)
